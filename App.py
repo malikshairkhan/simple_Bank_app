60e51e90f8bc59c681214545d4de02c6c5c867ac
HammadMaqbool <hammad.mqbl@gmail.com>
2023-02-08 10:25:36 +0500

    Indent Correction

--- App.py
+++ App.py
@@ -6,15 +6,24 @@ class user():
         self.CNIC = CNIC
     def showdetails(self):
         print("The name is:",self.Name, "\nAge :", str(self.Age), "\nGender: ", self.Gender, "\nCNIC is: " , str(self.CNIC))
+        
+    
+    
 class Bank(user):
+    
+    
     def __init__(self, Name, Age, Gender, CNIC):
         super().__init__(Name, Age, Gender, CNIC)
         self.balance = 0
+        
+        
     def Deposite(self):
         amount = int(input("Please enter the amount you want to deposite: "))
         self.amount = amount
         self.balance = self.balance+self.amount
         print("Updated balance is:", self.balance)
+        
+        
     def Withdraw(self):
         amount = int(input("Please enter the amount you want to withdraw: "))
         self.amount = amount
@@ -23,6 +32,8 @@ class Bank(user):
         else:
             self.balance = self.balance - self.amount
             print("Your Balance has been updated to : ", self.balance)
+            
+            
     def viewbalance(self):
         self.showdetails()
         print("Account balance is: ", self.balance)
